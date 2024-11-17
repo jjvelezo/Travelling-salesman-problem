56ea2dec0ff4d5b4ab52137e8881b006f8e73c2c
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2024-11-17 17:35:42 -0500

    Update grafRoute.py

--- python/grafRoute.py
+++ python/grafRoute.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-# Read coordinates from the file
+
 def read_coordinates(file_name):
     coordinates = []
     with open(file_name, 'r') as file:
@@ -9,21 +9,16 @@ def read_coordinates(file_name):
             coordinates.append((x, y))
     return coordinates
 
-# Function to plot the route
+
 def plot_route(coordinates, route):
-    # Extract coordinates in the order of the route
     x_vals = [coordinates[i][0] for i in route]
     y_vals = [coordinates[i][1] for i in route]
     
-    # Add the first point at the end to close the route
     x_vals.append(x_vals[0])
     y_vals.append(y_vals[0])
     
-    # Plot
-    plt.figure(figsize=(10, 10))
     plt.plot(x_vals, y_vals, marker='o', color='b', linestyle='-', markersize=5)
     
-    # Add numbers to each point
     for i, (x, y) in enumerate(coordinates):
         plt.text(x, y, str(i), fontsize=12, ha='right')
     
@@ -33,9 +28,7 @@ def plot_route(coordinates, route):
     plt.grid()
     plt.show()
 
-# Define the provided route
 route = []
 
-# Read coordinates from the file and plot the route
 coordinates = read_coordinates('Coord2.txt')
 plot_route(coordinates, route)
